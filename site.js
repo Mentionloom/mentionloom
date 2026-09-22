ef293985cf5b71560de7f169ff6273eccf4c0c88
Giovani Tier <giovanitier@gmail.com>
2026-09-22 10:34:54 +0300

    Refresh anchored globe asset

--- site.js
+++ site.js
@@ -1,7 +1,7 @@
 import { select } from "./app/lib/model.js";
 import { ACTIONS, ENGINES, QUESTIONS } from "./app/lib/data.js";
 import { initializeOrbit, enhance, number, animate, openDialog, closeDialog } from "./app/lib/ui.js";
-import { mountGlobe } from "./assets/globe.js?v=20260922-2";
+import { mountGlobe } from "./assets/globe.js?v=20260922-3";
 
 const $ = (s) => document.querySelector(s);
 const icon = (name) =>
